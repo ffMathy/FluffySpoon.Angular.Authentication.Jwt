255f5ff647c29da435fa773721ccea25031cdc2a
Mathias Lorenzen <mathias.lorenzen@live.com>
2017-05-01 19:00:52 +0200

    progress on intercepting.

--- src/FluffySpoonAuthenticationModule.ts
+++ src/FluffySpoonAuthenticationModule.ts
@@ -1,35 +1,45 @@
 ﻿import { NgModule, FactoryProvider, Injector } from '@angular/core';
 
+import { AuthenticationService } from './AuthenticationService';
+import { TokenContainer } from './TokenContainer';
+import { AuthenticationHttpInterceptor } from './AuthenticationHttpInterceptor';
+
+import { ExtendedHttp, FluffySpoonHttpModule } from 'fluffy-spoon.angular.http';
+
 export class FluffySpoonAuthenticationModule {
     static withUrls(
       anonymousTokenUrl: string,
       authenticatedTokenUrl: string,
       refreshTokenUrl: string): NgModule
     {
+        var authenticationService: AuthenticationService;
+
+        var httpInterceptor = new AuthenticationHttpInterceptor();
+
         return {
             providers: [
                 <FactoryProvider>{
-                    provide: HttpInterceptor,
-                    useFactory: interceptorType
-                },
-                <FactoryProvider>{
-                    provide: Http,
+                    provide: AuthenticationService,
                     useFactory: (
-                        connectionBackend: ConnectionBackend,
-                        httpInterceptor: HttpInterceptor) =>
+                        http: ExtendedHttp,
+                        tokenContainer: TokenContainer) =>
                     {
-                        return new ExtendedHttp(
-                            connectionBackend,
-                            new RequestOptions(),
-                            httpInterceptor);
+                        if (authenticationService) return authenticationService;
+                        return authenticationService = new AuthenticationService(
+                            http,
+                            tokenContainer,
+                            anonymousTokenUrl,
+                            authenticatedTokenUrl,
+                            refreshTokenUrl);
                     },
                     deps: [
-                        ConnectionBackend,
-                        HttpInterceptor
+                        ExtendedHttp,
+                        TokenContainer
                     ]
                 }
-            ]
-        }
+            ],
+            imports: [FluffySpoonHttpModule.withHttpInterceptor(httpInterceptor)]
+        };
     }
 
     static withDefaultUrls() {
