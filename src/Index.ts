8a647aec0ac717054ecf748970909cae902cfbb7
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2018-06-19 19:55:40 +0200

    Update Index.ts

--- src/Index.ts
+++ src/Index.ts
@@ -1,6 +1,4 @@
-export { AnonymousAuthenticationGuard } from './AnonymousAuthenticationGuard';
 export { AuthenticationHttpInterceptor } from './AuthenticationHttpInterceptor';
 export { AuthenticationService } from './AuthenticationService';
-export { CredentialAuthenticationGuard } from './CredentialAuthenticationGuard';
 export { FluffySpoonAuthenticationModule } from './FluffySpoonAuthenticationModule';
 export { TokenContainer } from './TokenContainer';
