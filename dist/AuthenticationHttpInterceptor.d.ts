469e1274c288f7d0efcde326d3bc75ba26db01fb
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-06-27 21:37:50 +0200

    removed dist folder.

--- dist/AuthenticationHttpInterceptor.d.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-import { HttpInterceptor, HttpHandler, HttpRequest, HttpEvent } from '@angular/common/http';
-import { TokenContainer } from './TokenContainer';
-export declare class AuthenticationHttpInterceptor implements HttpInterceptor {
-    private tokenContainer;
-    constructor(tokenContainer: TokenContainer);
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>;
-}
--- dist/Index.d.ts
+++
@@ -1,6 +0,0 @@
-export { AnonymousAuthenticationGuard } from './AnonymousAuthenticationGuard';
-export { AuthenticationHttpInterceptor } from './AuthenticationHttpInterceptor';
-export { AuthenticationService } from './AuthenticationService';
-export { CredentialAuthenticationGuard } from './CredentialAuthenticationGuard';
-export { FluffySpoonAuthenticationModule } from './FluffySpoonAuthenticationModule';
-export { TokenContainer } from './TokenContainer';
