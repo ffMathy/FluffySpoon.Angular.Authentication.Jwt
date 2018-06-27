8d42c98235ab8eff668c188c7e553abca13dbde7
Mathias Lykkegaard Lorenzen <mathias.lorenzen@live.com>
2018-06-27 21:36:57 +0200

    Delete AnonymousAuthenticationGuard.d.ts

--- dist/AnonymousAuthenticationGuard.d.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { AuthenticationService } from './AuthenticationService';
-import { TokenContainer } from './TokenContainer';
-export declare class AnonymousAuthenticationGuard implements CanActivate {
-    private authenticationService;
-    private tokenContainer;
-    constructor(authenticationService: AuthenticationService, tokenContainer: TokenContainer);
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean>;
-}
