cea186c690006dbd6a48ae0332a875394fda02d9
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-07-02 00:18:57 +0200

    changed module into function.

--- src/FluffySpoonAuthenticationModule.ts
+++ src/FluffySpoonAuthenticationModule.ts
@@ -6,50 +6,37 @@ import { AuthenticationHttpInterceptor } from './AuthenticationHttpInterceptor';
 
 import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 
-@NgModule({
-	declarations: [],
-	exports: []
-})
-export class FluffySpoonAuthenticationModule {
-	static withJwt(requestTokenUrl: string): ModuleWithProviders
-    {
-        var authenticationService: AuthenticationService;
-        var tokenContainer: TokenContainer;
-		
-		return {
-			ngModule: FluffySpoonAuthenticationModule,
-			providers: [
-				<ClassProvider>{
-					provide: HTTP_INTERCEPTORS,
-					useClass: AuthenticationHttpInterceptor,
-					multi: true
-				},
-                <FactoryProvider>{
-                    provide: TokenContainer,
-                    useFactory: () =>
-                    {
-                        if (tokenContainer) return tokenContainer;
-                        return tokenContainer = new TokenContainer();
-                    }
-                },
-                <FactoryProvider>{
-                    provide: AuthenticationService,
-                    useFactory: (
-                        http: HttpClient,
-                        tokenContainer: TokenContainer) =>
-                    {
-                        if (authenticationService) return authenticationService;
-                        return authenticationService = new AuthenticationService(
-                            http,
-							tokenContainer,
-							requestTokenUrl);
-                    },
-                    deps: [
-                        HttpClient,
-                        TokenContainer
-                    ]
-                }
+export function withJwt(requestTokenUrl: string) {
+	var authenticationService: AuthenticationService;
+	var tokenContainer: TokenContainer;
+	return [
+		<ClassProvider>{
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthenticationHttpInterceptor,
+			multi: true
+		},
+		<FactoryProvider>{
+			provide: TokenContainer,
+			useFactory: () => {
+				if (tokenContainer) return tokenContainer;
+				return tokenContainer = new TokenContainer();
+			}
+		},
+		<FactoryProvider>{
+			provide: AuthenticationService,
+			useFactory: (
+				http: HttpClient,
+				tokenContainer: TokenContainer) => {
+				if (authenticationService) return authenticationService;
+				return authenticationService = new AuthenticationService(
+					http,
+					tokenContainer,
+					requestTokenUrl);
+			},
+			deps: [
+				HttpClient,
+				TokenContainer
 			]
-        };
-    }
+		}
+	];
 }
