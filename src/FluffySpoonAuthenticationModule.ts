62076b2544057d3d2a25a02eab303f595892338e
Mathias Lorenzen <mathias.lorenzen@live.com>
2018-07-02 07:35:40 +0200

    upgraded module.

--- src/FluffySpoonAuthenticationModule.ts
+++ src/FluffySpoonAuthenticationModule.ts
@@ -1,4 +1,4 @@
-﻿import { NgModule, FactoryProvider, ClassProvider, Injector, ModuleWithProviders } from '@angular/core';
+﻿import { NgModule, FactoryProvider, ClassProvider, Injector, ModuleWithProviders, InjectionToken } from '@angular/core';
 
 import { AuthenticationService } from './AuthenticationService';
 import { TokenContainer } from './TokenContainer';
@@ -6,37 +6,54 @@ import { AuthenticationHttpInterceptor } from './AuthenticationHttpInterceptor';
 
 import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 
-export function withJwt(requestTokenUrl: string) {
-	var authenticationService: AuthenticationService;
-	var tokenContainer: TokenContainer;
-	return [
-		<ClassProvider>{
-			provide: HTTP_INTERCEPTORS,
-			useClass: AuthenticationHttpInterceptor,
-			multi: true
-		},
-		<FactoryProvider>{
-			provide: TokenContainer,
-			useFactory: () => {
-				if (tokenContainer) return tokenContainer;
-				return tokenContainer = new TokenContainer();
-			}
-		},
-		<FactoryProvider>{
-			provide: AuthenticationService,
-			useFactory: (
-				http: HttpClient,
-				tokenContainer: TokenContainer) => {
-				if (authenticationService) return authenticationService;
-				return authenticationService = new AuthenticationService(
-					http,
-					tokenContainer,
-					requestTokenUrl);
-			},
-			deps: [
-				HttpClient,
-				TokenContainer
+export const REQUEST_TOKEN_URL = new InjectionToken<string>('REQUEST_TOKEN_URL');
+
+let authenticationService: AuthenticationService;
+let tokenContainer: TokenContainer;
+
+export function getTokenContainer() {
+	if (tokenContainer) return tokenContainer;
+	return tokenContainer = new TokenContainer();
+}
+
+export function getAuthenticationService(http: HttpClient, tokenContainer: TokenContainer, tokenUrl: string) {
+	if (authenticationService) return authenticationService;
+	return authenticationService = new AuthenticationService(
+		http,
+		tokenContainer,
+		tokenUrl);
+}
+
+@NgModule({
+	declarations: [],
+	exports: []
+})
+export class FluffySpoonAuthenticationModule {
+	static forRoot(requestTokenUrl: string): ModuleWithProviders
+	{		
+		return {
+			ngModule: FluffySpoonAuthenticationModule,
+			providers: [
+				{ provide: REQUEST_TOKEN_URL, useValue: requestTokenUrl },
+				<ClassProvider>{
+					provide: HTTP_INTERCEPTORS,
+					useClass: AuthenticationHttpInterceptor,
+					multi: true
+				},
+                <FactoryProvider>{
+					provide: TokenContainer,
+					useFactory: getTokenContainer
+                },
+                <FactoryProvider>{
+					provide: AuthenticationService,
+					useFactory: getAuthenticationService,
+                    deps: [
+                        HttpClient,
+						TokenContainer,
+						REQUEST_TOKEN_URL
+                    ]
+                }
 			]
-		}
-	];
+        };
+    }
 }
